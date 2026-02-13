66ae8a5c48f57f143312aa8f178f68beddba4096
rafaelviktor29 <dev.rafaelviktor@gmail.com>
2026-02-12 22:59:47 -0300

    Recuperação das despesas no xlsx

--- src/CashFlow.Domain/Repositories/Expenses/IExpensesReadOnlyRepository.cs
+++ src/CashFlow.Domain/Repositories/Expenses/IExpensesReadOnlyRepository.cs
@@ -2,8 +2,9 @@
 
 namespace CashFlow.Domain.Repositories.Expenses;
 
-public interface IExpensesReadOnlyRepository // To Do: terminar essa porra
+public interface IExpensesReadOnlyRepository
 {
     Task<List<Expense>> GetAll();
     Task<Expense?> GetById(long id);
+    Task<List<Expense>> FilterByMonth(DateOnly date);
 }
